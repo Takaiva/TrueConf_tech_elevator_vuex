d9328a2d12e85b114661eab3549c87afcb3ecea4
Александр Сдобников <sdobnikove@mail.ru>
2022-12-18 14:34:29 +0300

    add floor component

---
+++ src/components/FloorComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="floor__item">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+.floor__item {
+  width: 120px;
+  height: 120px;
+  background: #00a0b0;
+  border: 1px solid orange;
+  padding: 5px;
+  border-radius: 8px;
+}
+</style>

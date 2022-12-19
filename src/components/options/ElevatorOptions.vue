df6eaf752c50c68490c6c54c54255e79d784a2c5
Александр Сдобников <sdobnikove@mail.ru>
2022-12-19 07:19:12 +0300

    following component approach, assemble "app options" from "floor options" and "elevator options" components

---
+++ src/components/options/ElevatorOptions.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="options__elevators">
+    <div class="options__name">Количество лифтов</div>
+    <form>
+      <button :disabled="elevatorsCount.length <= 1" @click.prevent="minusElevator">
+        –
+      </button>
+      <label for="elevators">
+        <input disabled type="number" :value="elevatorsCount.length" min="2" id="elevators"/>
+      </label>
+      <button @click.prevent="plusElevator">+</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations, mapState } from 'vuex';
+
+export default {
+  methods: {
+    ...mapMutations({
+      plusElevator: 'options/plusElevator',
+      minusElevator: 'options/minusElevator',
+    }),
+  },
+  computed: {
+    ...mapState({
+      elevatorsCount: (state) => state.options.elevatorsCount,
+    }),
+    ...mapGetters({
+      getElevatorsCount: 'options/getElevatorsCount',
+    }),
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/options/FloorOptions.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="options__floors">
+    <div class="options__name">Количество этажей</div>
+    <form>
+      <button :disabled="floorsCount.length <= 2" @click.prevent="minusFloor">
+        –
+      </button>
+      <label for="floors">
+        <input disabled type="number" :value="floorsCount.length" min="2" id="floors"/>
+      </label>
+      <button @click.prevent="plusFloor">+</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations, mapState } from 'vuex';
+
+export default {
+  methods: {
+    ...mapMutations({
+      plusFloor: 'options/plusFloor',
+      minusFloor: 'options/minusFloor',
+    }),
+  },
+  computed: {
+    ...mapState({
+      floorsCount: (state) => state.options.floorsCount,
+    }),
+    ...mapGetters({
+      getFloorsCount: 'options/getFloorsCount',
+    }),
+  },
+};
+</script>
+
+<style>
+</style>
